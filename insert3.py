cd363851cb5c20ee582d4aef5a8f5950c2cc516b
JinyangLi0228 <lijinyang@pku.edu.cn>
2020-05-01 03:23:08 +0800

    initial commit

---
+++ insert3.py
@@ -0,0 +1,21 @@
+import pymysql
+
+db = pymysql.connect(host='localhost', port=3306,
+                     user='root', passwd='greeN7^Tr33', db='end2end', charset='utf8')
+
+cursor = db.cursor()
+
+sql = "INSERT INTO py3(ID, Age, Name) VALUES (%s,%s,%s)"
+T = []
+
+for i in range(1, 1000001):
+    T.append((i, 20, 'Name'))
+
+
+try:
+    cursor.executemany(sql, T)
+    db.commit()
+except:
+    db.rollback()
+cursor.close()
+db.close()
---
+++ server/swap1M.py
@@ -0,0 +1,35 @@
+import pymysql
+import string
+import random
+
+db = pymysql.connect(host='localhost', port=3306,
+                     user='root', passwd='greeN7^Tr33', db='end2end', charset='utf8')
+
+cursor = db.cursor()
+
+sql = "INSERT INTO swap100(ID, Age, Name, c4, c5, c6, c7, c8, c9, c10) VALUES (%s,%s,%s, %s,%s,%s, %s,%s,%s, %s)"
+T = []
+
+def string_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+for i in range(1, 1000001):
+    age = random.randint(1,50)
+    name = ''.join(random.sample(string.ascii_letters + string.digits, random.randint(1, 30)))
+    c4 = ''.join(random.sample(string.ascii_letters + string.digits, random.randint(1, 30)))
+    c5 = ''.join(random.sample(string.ascii_letters + string.digits, random.randint(1, 30)))
+    c6 = ''.join(random.sample(string.ascii_letters + string.digits, random.randint(1, 30)))
+    c7 = ''.join(random.sample(string.ascii_letters + string.digits, random.randint(1, 30)))
+    c8 = ''.join(random.sample(string.ascii_letters + string.digits, random.randint(1, 30)))
+    c9 = ''.join(random.sample(string.ascii_letters + string.digits, random.randint(1, 30)))
+    c10 = ''.join(random.sample(string.ascii_letters + string.digits, random.randint(1, 30)))
+    T.append((i, age, name, c4, c5, c6, c7, c8, c9, c10))
+
+
+try:
+    cursor.executemany(sql, T)
+    db.commit()
+except:
+    db.rollback()
+cursor.close()
+db.close()
